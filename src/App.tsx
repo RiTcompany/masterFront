11c8221f5be97fd784ddf4f5bbada2012dadf042
dashamalkina <dashkamalkina01@yandex.com>
2024-10-01 01:11:13 +0300

    feat: add style files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
-import {Route, Routes, useLocation} from "react-router-dom";
-import {Navbar} from "./components/Navbar/Navbar.tsx";
+import {Route, Routes,Navigate, useLocation} from "react-router-dom";
 import {Main} from "./pages/Main/Main.tsx";
 import {Registration} from "./pages/Registration/Registration.tsx";
 import {Profile} from "./pages/Profile/Profile.tsx";
@@ -9,6 +8,13 @@ import {NewOrder} from "./pages/NewOrder/NewOrder.tsx";
 import {TaskPage} from "./pages/TaskPage/TaskPage.tsx";
 import {Footer} from "./components/Footer/Footer.tsx";
 import {CookieConsent} from "./components/CookieConsent/CookieConsent.tsx";
+
+import LayoutAdmin from "./components/admin/layout/layout-admin.tsx";
+import AdminClient from "./components/admin/client/adminClient.tsx";
+import MastersComponent from "./components/admin/masters/mastersComponent.tsx";
+import {Navbar} from "./components/Navbar/Navbar.tsx";
+
+
 
 export function parseJwt(token: string) {
     try {
@@ -40,17 +46,32 @@ function App() {
             // setAuthUserRole(data.role)
         }
     }, []);
+    const hideNavbar = location.pathname.startsWith('/admin');
 
   return (
     <>
-      <Navbar/>
+        {!hideNavbar && <Navbar />}
+
+        <Routes>
+            <Route path="/admin" element={<LayoutAdmin />}>
+                <Route path="" element={<Navigate to="/admin/clients" />} />
+                <Route path="/admin/clients" element={<AdminClient/>}/>
+                <Route path="/admin/masters" element={<MastersComponent/>}/>
+            </Route>
+
+        </Routes>
+
       <Routes>
+
         <Route path={'/'} element={<Main />}/>
         <Route path={'/registration'} element={<Registration/>}/>
         <Route path="/profile/:id" element={<Profile authUserId={authUserId}/>} />
         <Route path="/create-order" element={<NewOrder />} />
         <Route path="/task/:id" element={<TaskPage />} />
+
+
       </Routes>
+
         {location.pathname === "/" &&
             <Footer/>
         }
