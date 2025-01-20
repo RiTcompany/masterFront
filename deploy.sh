f4a6b55d657183af68468ef67992082115432908
Michael Nazaryan <72793688+MkhlNzrn@users.noreply.github.com>
2025-01-20 15:04:10 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+nvm install node
+
+git pull
+
+rm -r dist
+
+npm run build
+
+rm -r /root/project/backend/master-for-an-hour-backend/src/main/resources/static
+
+mkdir /root/project/backend/master-for-an-hour-backend/src/main/resources/static
+
+cp -rp /root/project/frontend/masterFront/dist/* /root/project/backend/master-for-an-hour-backend/src/main/resources/static
